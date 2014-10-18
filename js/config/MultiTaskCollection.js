5432fcc9077f3cdca578c6e05f3716759ba13c31
Dan Stocker <dan@kwaia.com>
2014-10-18 10:27:27 +0100

    adds documentation to MultiTaskCollection

--- js/config/MultiTaskCollection.js
+++ js/config/MultiTaskCollection.js
@@ -6,13 +6,21 @@ troop.postpone(grocer, 'MultiTaskCollection', function () {
         self = base.extend();
 
     /**
+     * Creates a MultiTaskCollection instance.
+     * MultiTaskCollection instances may also be created via conversion from Array and Hash.
+     * (In fact those are favorable to .create().)
      * @name grocer.MultiTaskCollection.create
      * @function
      * @param {Object|Array} items
      * @returns {grocer.MultiTaskCollection}
+     * @see Array#toMultiTaskCollection
+     * @see sntls.Hash#toMultiTaskCollection
      */
 
     /**
+     * The MultiTaskCollection class implements a typed collection for storing and managing
+     * MultiTask instances. The main purpose of MultiTaskCollection is to provide conversion to
+     * config-related objects and classes.
      * @class
      * @extends sntls.Collection
      * @extends grocer.MultiTask
@@ -20,7 +28,9 @@ troop.postpone(grocer, 'MultiTaskCollection', function () {
     grocer.MultiTaskCollection = self
         .addMethods(/** @lends grocer.MultiTaskCollection# */{
             /**
-             * @param {string} [targetPrefix]
+             * Generates a grunt config object for all tasks in the collection,
+             * with targets optionally prefixed.
+             * @param {string} [targetPrefix] Optional prefix for all targets of all tasks.
              * @returns {Object|Array}
              */
             getConfigNode: function (targetPrefix) {
@@ -28,7 +38,8 @@ troop.postpone(grocer, 'MultiTaskCollection', function () {
             },
 
             /**
-             * @param {string} [targetPrefix]
+             * Converts task collection to a GruntConfig instance.
+             * @param {string} [targetPrefix] Optional prefix for all targets of all tasks.
              * @returns {grocer.GruntConfig}
              */
             toGruntConfig: function (targetPrefix) {
@@ -41,7 +52,10 @@ troop.amendPostponed(sntls, 'Hash', function () {
     "use strict";
 
     sntls.Hash.addMethods(/** @lends sntls.Hash# */{
-        /** @returns {grocer.MultiTaskCollection} */
+        /**
+         * Converts Hash to MultiTaskCollection. Hash items must be MultiTask instances.
+         * @returns {grocer.MultiTaskCollection}
+         */
         toMultiTaskCollection: function () {
             return grocer.MultiTaskCollection.create(this.items);
         }
@@ -54,7 +68,10 @@ troop.amendPostponed(sntls, 'Hash', function () {
     troop.Properties.addProperties.call(
         Array.prototype,
         /** @lends Array# */{
-            /** @returns {grocer.MultiTaskCollection} */
+            /**
+             * Converts Array to MultiTaskCollection. Array items must be MultiTask instances.
+             * @returns {grocer.MultiTaskCollection}
+             */
             toMultiTaskCollection: function () {
                 return grocer.MultiTaskCollection.create(this);
             }
